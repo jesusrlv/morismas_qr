48aa5c1b43f20058bb608cce341524d05503c979
jesusrlv <jesusrlvrojo@gmail.com>
2022-05-04 13:05:44 -0500

    json done

--- perfil/prcd/qc/qc.php
+++ perfil/prcd/qc/qc.php
@@ -1,7 +1,7 @@
 <?php
 
     $servername="localhost";
-    $database="tenis"; //solo se quitó para conexión remota
+    $database="morismas"; //solo se quitó para conexión remota
     $username="root";
     $password="";
 
