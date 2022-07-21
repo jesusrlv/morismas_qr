fa57ef500b515d3586cd87090e081bb7e302988c
jesus rlv <jesusrlv@iMac-de-jesus.local>
2022-07-21 17:51:06 -0500

    server prueba

--- prcd/qc/qc.php
+++ prcd/qc/qc.php
@@ -1,9 +1,15 @@
 <?php
 
     $servername="localhost";
-    $database="c2morismas"; //solo se quitó para conexión remota
-    $username="c2cOnf_psic_0307";
-    $password="ThmfMTk_G6";
+    $database="morismas"; //solo se quitó para conexión remota
+    $username="admin";
+    $password="";
+
+    // server prueba
+    // $servername="localhost";
+    // $database="c2morismas"; //solo se quitó para conexión remota
+    // $username="c2cOnf_psic_0307";
+    // $password="ThmfMTk_G6";
 
     $conn= new mysqli ($servername,$username,$password,$database); //solo se quitó para conexión remota
 
